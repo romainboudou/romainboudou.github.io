685460743833f3973b1dc0f556394eaf988159f0
Romain Boudou <118806640+romainboudou@users.noreply.github.com>
2025-01-15 18:00:46 +0100

    Retouches

--- data/projects.js
+++ data/projects.js
@@ -4,7 +4,7 @@ const projects = [
         title: "The Nine Winds",
         date: "2024",
         description: "The Nine Winds is the debut of an atmospheric Metroidvania game steeped in ancient mythology, developed in just one week.",
-        detailedDescription: "As Ketzal, you are sent by the god Quetzalcoatl to recover the lost Nine Winds, spiritual essences tied to balance and harmony. Scattered across temples and underground labyrinths, each Wind represents an aspect of a fractured world: Wisdom, Peace, Nature, Justice, Time, Love, Memory, Soul, and Unity.",
+        detailedDescription: "<p>As Ketzal, you are sent by the god Quetzalcoatl to recover the lost Nine Winds, spiritual essences tied to balance and harmony. Scattered across temples and underground labyrinths, each Wind represents an aspect of a fractured world: <strong>Wisdom</strong>, <strong>Peace</strong>, <strong>Nature</strong>, <strong>Justice</strong>, <strong>Time</strong>, <strong>Love</strong>, <strong>Memory</strong>, <strong>Soul</strong>, and <strong>Unity</strong>.</p><h4>Features:</h4><ul><li>Explore interconnected map filled with secrets, puzzles, and challenges.</li><li>Gain unique abilities by recovering the Winds, allowing you to traverse new areas.</li><li>Uncover the story of the Nine Devotees and the tragic downfall of the world's balance.</li><li>Unique handmade designs.</li><li>Playable entirely in French.</li></ul><p>Will you choose to restore the balance and reset the world, or will you forge a new path? The winds call, and your destiny awaits.</p>",
         teamSize: 1,
         time: "1 week",
         tools: "Unity",
