904212d38d4827db4291b17e4e890d4cde751398
romainboudou <118806640+romainboudou@users.noreply.github.com>
2025-01-15 01:22:52 +0100

    first element

--- data/projects.js
+++ data/projects.js
@@ -3,16 +3,19 @@ const projects = [
         id: "nine-winds",
         title: "The Nine Winds",
         date: "2024",
-        description: "An immersive game project developed with Unity.",
-        detailedDescription: "The Nine Winds is an adventure game where players explore a mystical world filled with puzzles and challenges. Developed solo using Unity, the game was completed in one week.",
+        description: "The Nine Winds is the debut of an atmospheric Metroidvania game steeped in ancient mythology, developed in just one week.",
+        detailedDescription: "As Ketzal, you are sent by the god Quetzalcoatl to recover the lost Nine Winds, spiritual essences tied to balance and harmony. Scattered across temples and underground labyrinths, each Wind represents an aspect of a fractured world: Wisdom, Peace, Nature, Justice, Time, Love, Memory, Soul, and Unity.",
         teamSize: 1,
         time: "1 week",
         tools: "Unity",
         category: "game-programming",
         image: "../img/the-nine-winds-thumbnail.png",
         photos: [
-            "./images/the-nine-winds-1.jpg",
-            "./images/the-nine-winds-2.jpg"
+            "../img/the-nine-winds-1.png",
+            "../img/the-nine-winds-2.png",
+            "../img/the-nine-winds-3.png",
+            "../img/the-nine-winds-4.png",
+            "../img/the-nine-winds-5.png"
         ],
         link: "./project.html?id=nine-winds",
     },
@@ -28,8 +31,11 @@ const projects = [
         category: "web-programming",
         image: "./images/web-dashboard-thumbnail.jpg",
         photos: [
-            "./images/web-dashboard-1.jpg",
-            "./images/web-dashboard-2.jpg"
+            "./images/web-dashboard-1.png",
+            "./images/web-dashboard-2.png",
+            "./images/web-dashboard-3.png",
+            "./images/web-dashboard-4.png",
+            "./images/web-dashboard-5.png"
         ],
         link: "./project.html?id=web-dashboard",
     },
